58222bfe2a343f188369be259b240c3da9106149
Ryo Takahashi <31044@ojs.ac.jp>
2020-07-14 14:00:35 +0900

    データベースの内容表示

--- ADOsample/Form1.cs
+++ ADOsample/Form1.cs
@@ -16,5 +16,24 @@ namespace ADOsample
         {
             InitializeComponent();
         }
+
+        private void memoTableBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.memoTableBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.infosys202025DataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.memoTableTableAdapter.Fill(this.infosys202025DataSet.memoTable);
+        }
     }
 }
